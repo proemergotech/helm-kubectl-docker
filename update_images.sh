707066c92c5ef16bf9e6170db3fb16bba7012d1e
Norbert Csibra <norbert.csibra@proemergotech.com>
2019-09-19 08:44:23 +0200

    Update to handle multiple resorce with the same image

--- update_images.sh
+++ update_images.sh
@@ -5,25 +5,24 @@ set -euo pipefail
 # stop script not just the current command
 trap "exit" SIGINT SIGTERM
 
-resource=$(kubectl -n ${CI_ENVIRONMENT_NAME} get deployment,daemonset,statefulset -l app=${CI_PROJECT_NAME} -o json)
-item_count=$(echo ${resource} | jq -r '.items | length') 
+resources=$(kubectl -n ${CI_ENVIRONMENT_NAME} get deployment,daemonset,statefulset -l dliver.com/project=${CI_PROJECT_NAME} -o json)
+item_count=$(jq -r '.items | length' <<< ${resources}) 
 
-if [[ ${item_count} > 1 ]]; then
-    echo "Found more than 1 resource: "
-    echo "$(echo ${resource} | jq -r '.items[] | .kind + "/" + .metadata.name')"
-    exit 1
-elif [[ ${item_count} == 0 ]]; then
-    echo "Resource '${CI_PROJECT_NAME}' not found"
+if [[ ${item_count} == 0 ]]; then
+    echo "Resource for project '${CI_PROJECT_NAME}' not found"
     exit 1
 fi
 
-resource_kind=$(echo ${resource} | jq -r '.items[].kind')
-container_names=($(echo ${resource} | jq -r '.items[].spec.template.spec | .containers[],.initContainers[] | select(.image | contains($ENV.CI_PROJECT_NAME)) | .name' ))
+for (( i = 0; i < ${item_count}; i++ )); do
+    resource=$(jq -r ".items[$i]" <<< ${resources})
+    resource_identifier=$(jq -r '.kind + "/" + .metadata.name' <<< ${resource})
+    container_names=($(jq -r '.spec.template.spec | .containers[],.initContainers[]? | select(.image | contains($ENV.CI_PROJECT_NAME)) | .name' <<< ${resource}))
+    
+    image_updates=()
+    for container in ${container_names[@]} 
+    do
+        image_updates+=("${container}=${CI_REGISTRY}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}:${CI_COMMIT_SHA}")
+    done
 
-image_updates=()
-for container in ${container_names[@]} 
-do
-    image_updates+=("${container}=${CI_REGISTRY}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}:${CI_COMMIT_SHA}")
+    kubectl -n ${CI_ENVIRONMENT_NAME} set image ${resource_identifier} ${image_updates[@]} 
 done
-
-kubectl -n ${CI_ENVIRONMENT_NAME} set image ${resource_kind}/${CI_PROJECT_NAME} ${image_updates[@]} 
